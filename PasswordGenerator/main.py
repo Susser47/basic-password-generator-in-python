5f2b423bc308f18a66163e9f7055e616a5521442
Susser47 <139006122+Susser47@users.noreply.github.com>
2024-01-26 18:20:32 +0100

    added comments to the main.py to make code more readable

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,25 +1,24 @@
 import random
 
-# lista di caratteri
+# lista di caratteri - caracter list
 characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
-# chiede il numero di caratteri desiderati e riprova a chiedere se l'input non è un numero
 while True:
-    caratteri = input("numero caratteri: ")
+    caratteri = input("numero caratteri: ") # chiede il numero di carateri - asks for the lenght of the password
 
-    if not caratteri.isdigit():
+    if not caratteri.isdigit():    # controlla se la variabile lenght è un int senno fa vedere un errore - checks if the "lenght" variable is not int and displays error
         print("metti un numero e non una stringa")
         input("premi invio per riprovare")
 
-    else:
+    else:    # se non c'è errore trasforma la variabile in int - if there is no error it turns the caracter into int type
         caratteri = int(caratteri)
         break
 
-# crea la variabile vuota a cui dopo si aggiungeranno i caratteri
+# crea la variabile vuota a cui dopo si aggiungeranno i caratteri - creates the variable that characters will be added to
 password = ""
 
 for i in range(caratteri):
-    password += random.choice(characters)
+    password += random.choice(characters)    # crea e agginge caratteri alla variabile password - creates and adds characters to the password variable
 
 print(password)
 

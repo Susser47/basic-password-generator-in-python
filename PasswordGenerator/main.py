5a70132f18003606bc30309b9877c61603f6a377
Susser47 <139006122+Susser47@users.noreply.github.com>
2024-03-22 16:40:41 +0100

    fixed bugs in naming variables

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,25 +1,25 @@
 import random
 
 # lista di caratteri - caracter list
-Characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 while True:
-    Caratteri = input("numero caratteri: ") # chiede il numero di carateri - asks for the lenght of the password
+    caratteri = input("numero caratteri: ") # chiede il numero di carateri - asks for the lenght of the password
 
-    if not Caratteri.isdigit():    # controlla se la variabile lenght è un int senno fa vedere un errore - checks if the "lenght" variable is not int and displays error
+    if not caratteri.isdigit():    # controlla se la variabile lenght è un int senno fa vedere un errore - checks if the "lenght" variable is not int and displays error
         print("metti un numero e non una stringa")
         input("premi invio per riprovare")
 
     else:    # se non c'è errore trasforma la variabile in int - if there is no error it turns the caracter into int type
-        Caratteri = int(Caratteri)
+        caratteri = int(caratteri)
         break
 
 # crea la variabile vuota a cui dopo si aggiungeranno i caratteri - creates the variable that characters will be added to
-Password = ""
+password = ""
 
 for i in range(caratteri):
-    Password += random.choice(characters)    # crea e agginge caratteri alla variabile password - creates and adds characters to the password variable
+    password += random.choice(characters)    # crea e agginge caratteri alla variabile password - creates and adds characters to the password variable
 
-print(Password)
+print(password)
 
 input("premi invio per chiudere il programma")
